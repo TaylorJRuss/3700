49e69376f439157c898b5e30b3a08de589818288
TaylorJRuss <Justinrussell384@gmail.com>
2024-11-01 17:08:04 -0400

    Update util.py

--- HW/util.py
+++ HW/util.py
@@ -2,38 +2,36 @@ import psycopg2
 from psycopg2 import Error
 
 # this function is based on the tutorial at: https://pynative.com/python-postgresql-tutorial/
-def connect_to_db(username='ruiwu1990',password='test',host='127.0.0.1',port='5432',database='dvdrental'):
+def connect_to_db(username='raywu1990',password='test',host='127.0.0.1',port='5432',database='dvdrental')
 	try:
 	    # Connect to an existing database
 	    connection = psycopg2.connect(user=username,
-	                                  password=password,
-	                                  host=host,
-	                                  port=port,
-	                                  database=database)
+                                      password=password,
+                                      host=host,
+                                      port=port,
+                                      database=database)
 
-	    # Create a cursor to perform database operations
-	    cursor = connection.cursor()
-	    print("connected to the database")
+        # Create a cursor to perform database operations
+        cursor = connection.cursor()
+        print("connected to the database")
 
-	    return cursor, connection
+        return cursor, connection
 
-	except (Exception, Error) as error:
-	    print("Error while connecting to PostgreSQL", error)
-	
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
 
-def disconnect_from_db(connection,cursor):
-    if (connection):
+
+def disconnect_from_db(connection, cursor):
+    if connection:
         cursor.close()
         connection.close()
         print("PostgreSQL connection is closed.")
     else:
-    	print("Connection does not work.")
+        print("Connection does not work.")
 
 
-
-# run_sql(cursor,"select from;")
 def run_and_fetch_sql(cursor, sql_string=""):
-try:
+    try:
         cursor.execute(sql_string)
         record = cursor.fetchall()
         return record
