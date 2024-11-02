646f452544e8a470e17f063cab9e07f97b78ad9a
TaylorJRuss <Justinrussell384@gmail.com>
2024-11-02 15:22:12 -0400

    Update util.py

--- HW/util.py
+++ HW/util.py
@@ -1,3 +1,7 @@
+dimport psycopg2
+from psycopg2 import Error
+
+# this function is based on the tutorial at: https://pynative.com/python-postgresql-tutorial/
 def connect_to_db(username='russtay', password='database', host='127.0.0.1', port='5432', database='dvdrental'):
     try:
         # Connect to an existing database
@@ -5,7 +9,13 @@ def connect_to_db(username='russtay', password='database', host='127.0.0.1', por
 
         # Create a cursor to perform database operations
         cursor = connection.cursor()
-@@ -23,7 +19,7 @@ def connect_to_db(username='russtay', password='database', host='127.0.0.1', por
+        print("connected to the database")
+
+        return cursor, connection
+
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+
 
 def disconnect_from_db(connection, cursor):
     if connection:
@@ -13,7 +23,14 @@ def disconnect_from_db(connection, cursor):
         connection.close()
         print("PostgreSQL connection is closed.")
     else:
-@@ -38,8 +34,3 @@ def run_and_fetch_sql(cursor, sql_string=""):
+        print("Connection does not work.")
+
+
+def run_and_fetch_sql(cursor, sql_string=""):
+    try:
+        cursor.execute(sql_string)
+        record = cursor.fetchall()
+        return record
     except (Exception, Error) as error:
         print("Errors while executing the code: ", error)
         return -1
