5a251034e33c63e2bf56f510d5dd87e0d8da43fe
TaylorJRuss <Justinrussell384@gmail.com>
2024-11-01 23:45:31 -0400

    Update util.py

--- HW/util.py
+++ HW/util.py
@@ -12,29 +12,29 @@ def connect_to_db(username='russtay',password='database',host='127.0.0.1',port='
                                       database=database)
 
         # Create a cursor to perform database operations
-        cursor = connection.cursor()
-        print("connected to the database")
+        cursor = connection.cursor() 
+        print("connected to the database") 
 
-        return cursor, connection
+        return cursor, connection 
 
-    except (Exception, Error) as error:
-        print("Error while connecting to PostgreSQL", error)
-
-
-def disconnect_from_db(connection, cursor):
-    if connection:
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed.")
-    else:
-        print("Connection does not work.")
+    except (Exception, Error) as error: 
+        print("Error while connecting to PostgreSQL", error) 
 
 
-def run_and_fetch_sql(cursor, sql_string=""):
-    try:
-        cursor.execute(sql_string)
-        record = cursor.fetchall()
-        return record
-    except (Exception, Error) as error:
-        print("Errors while executing the code: ", error)
-        return -1
+def disconnect_from_db(connection, cursor): 
+    if connection: 
+        cursor.close() 
+        connection.close() 
+        print("PostgreSQL connection is closed.") 
+    else: 
+        print("Connection does not work.") 
+
+
+ddef run_and_fetch_sql(cursor, sql_string=""): 
+    try: 
+        cursor.execute(sql_string) 
+        record = cursor.fetchall() 
+        return record 
+    except (Exception, Error) as error: 
+        print("Errors while executing the code: ", error) 
+        return -1 
